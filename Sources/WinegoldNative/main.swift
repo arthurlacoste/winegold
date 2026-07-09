fb0865a9e9b2998f6d8708e6af0d0ccb5efdf3e3
Arthur Lacoste <arthurlacoste@users.noreply.github.com>
2026-07-09 18:40:56 +0200

    chore: silence NSApplicationMain warning

--- Sources/WinegoldNative/main.swift
+++ Sources/WinegoldNative/main.swift
@@ -3,4 +3,4 @@ import Cocoa
 let app = NSApplication.shared
 let delegate = AppDelegate()
 app.delegate = delegate
-NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
+_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
